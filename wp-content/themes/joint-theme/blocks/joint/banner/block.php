47071d4071c31cb6b93b7f32f3eaebc482df4b7a
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-10-19 11:58:01 +0100

    Add banner

--- wp-content/themes/joint-theme/blocks/joint/banner/block.php
+++ wp-content/themes/joint-theme/blocks/joint/banner/block.php
@@ -9,20 +9,24 @@ Post Types: null
 Allow Multiple: true
 */
 
-$image		= get_field('banner_image');
-$content	= get_field('banner_content');
-$button		= get_field('banner_button');
-$bkg 		= get_field('banner_content_bkg');
+$image			= get_field('banner_image');
+$mobileimage	= get_field('banner_image_mobile');
+$content		= get_field('banner_content');
+
+$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/button' );
+// $button		= get_field('banner_button');
+// $bkg 		= get_field('banner_content_bkg');
 ?>
-<section class="banner text--white<?php if ( $bkg == '1' ) { ?> f-aligncenter<?php } ?>" <?php if ( $image['large'] ) { ?>style="background-image: url('<?php echo $image['large']['url']; ?>')"<?php } ?>>
+<section class="banner">
 	<div class="banner__content">
-		<?php if ( $bkg == '1' ) { ?><div class="banner__content--bkg"><?php } ?>
-		<?php if ( $content ) { ?><h1><?php echo $content; ?></h1><?php } ?>
-		<?php if ($button) { ?>
-			<a class="btn btn--block" href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>"><?php echo $button['title']; ?></a>
-		<?php } ?>
-		<?php if ( $bkg == '1' ) { ?></div><?php } ?>
+		<?php echo '<InnerBlocks allowedBlocks="' . esc_attr( wp_json_encode( $allowed_blocks ) ) . '" />'; ?>
+		<?php if ( $content ) { ?><?php echo $content; ?><?php } ?>
+		
+		<?php if ( $image ) { ?><img loading="lazy" class="banner__image<?php if ( $mobileimage ) { ?> hidemobile<?php } ?>" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /><?php } ?>
+		<?php if ( $mobileimage ) { ?><img loading="lazy" class="banner__image hidedesktop" src="<?php echo $mobileimage['url']; ?>" alt="<?php echo $mobileimage['alt']; ?>" /><?php } ?>
 	</div>
+	
+
 	<?php if ( $image['large'] ) { ?><img loading="lazy" class="banner__image hidemobile" src="<?php echo $image['large']['url']; ?>" alt="<?php echo $image['large']['alt']; ?>" /><?php } ?>
 	<?php if ( $image['mobile'] ) { ?><img loading="lazy" class="banner__image hidedesktop" src="<?php echo $image['mobile']['url']; ?>" alt="<?php echo $image['mobile']['alt']; ?>" /><?php } ?>
 </section>

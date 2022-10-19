35c4921ff5f63af42092cbb207fb1428b5f2f3ea
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-10-19 17:47:47 +0100

    Style footer

--- wp-content/themes/joint-theme/footer.php
+++ wp-content/themes/joint-theme/footer.php
@@ -1,6 +1,7 @@
-</div><!-- /.global-container -->
+
 <?php get_template_part('partials/site', 'footer'); ?>
 <?php get_template_part('partials/global', 'cookie-notice'); ?>
 <?php wp_footer(); ?>
+</div><!-- /.global-container -->
 </body>
 </html>

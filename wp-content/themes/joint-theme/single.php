bd7a86aa3c260fe38c6eef8892bcd6ca07f624cb
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-10-25 17:37:12 +0100

    Add similar property

--- wp-content/themes/joint-theme/single.php
+++ wp-content/themes/joint-theme/single.php
@@ -139,12 +139,127 @@
 					</div>
 				<?php } ?>
 
-				MAP<br />
 				FORM<br />
 
-				Similar properties<br />
+				<p><strong>Similar properties</strong></p>
+				<?php
+					$properties = new WP_Query( array(
+						'post_type' => 'accommodation',
+						'posts_per_page' => 1,
+						'orderby' => 'rand',
+						'post__not_in' => array( $post->ID ),
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'location',
+								'terms' => $accommodation,
+							),
+						),
 
-				<a href="<?php esc_url(the_permalink() ); ?>" class="btn">Find out more</a>
+					));
+				?>
+
+				<?php if ($properties->have_posts()) { ?>
+					<div class="accommodation-featured">
+					<?php while($properties->have_posts()) : $properties->the_post(); ?>
+					<?php
+						$details = get_field('property_details', get_the_id());
+						$postcode = $details['postcode'];
+						$rooms = $details['total_rooms'];
+						$available = $details['available_rooms'];
+						$availabledate = $details['available_from'];
+						$price = $details['price'];
+						$pricebills = $details['price_bills'];
+						$shortdesc = $details['short_description'];
+
+						$features = get_field('features', get_the_id());
+					?>
+						<div class="accommodation__item">
+							<div class="accommodation__image">
+							<?php if( get_the_post_thumbnail() ) { ?>	
+								<?php echo get_the_post_thumbnail(); ?>
+							<?php } else { ?>
+								<img src="<?php echo get_template_directory_uri(); ?>/src/images/property-default-thumb.png" alt="Property default image" />
+							<?php } ?>
+							</div>
+							<div class="accommodation__content">
+								<h4 class="text--orange text--body text--bold"><?php the_title(); ?>, 
+									<?php foreach ( get_the_terms( get_the_ID(), 'location' ) as $location ) { ?><?php echo $location->name ; ?><?php } ?>
+								</h4>
+								<div class="accommodation__details">
+									<div>
+										<div class="accommodation__rooms">
+											<?php if($rooms) { ?><p><strong><?php echo $rooms; ?> room<?php if($rooms > '1') { echo 's'; } ?> remaining<?php } ?></strong></p>
+											<?php if($available) { ?><p><?php echo $available; ?> bedroom house</p><?php } ?>
+										</div>
+										<div class="accommodation__amenities">
+										<?php if( have_rows('amenities', get_the_id()) ): ?>
+											<?php while( have_rows('amenities', get_the_id()) ): the_row(); ?>
+												<?php if( have_rows('amenity', get_the_id()) ): ?>
+													<?php while( have_rows('amenity', get_the_id()) ): the_row(); 
+														$amenity = get_sub_field('amenity');
+														$distance = get_sub_field('amenity_distance');
+													?>
+													<div class="accommodation__amenities-item">
+														<p><strong><?php echo $amenity; ?></strong><br />
+														<?php echo $distance; ?> </p>
+													</div>
+													<?php endwhile; ?>
+												<?php endif; ?>
+											<?php endwhile; ?>
+										<?php endif; ?>
+										</div>
+										<?php if($availabledate) { ?><p><strong>Available</strong><br/>
+											<?php echo $availabledate; ?></p><?php } ?>
+									</div>
+									<div>
+										<div class="accommodation__price">
+											<?php if($price) { ?><p><strong><?php echo $price; ?><?php } ?></strong></p>
+											<?php if($pricebills) { ?><p>Inc. bills: <?php echo $pricebills; ?></p><?php } ?>
+										</div>
+
+										<?php if($shortdesc) { ?>
+											<div class="accommodation__features">
+												<?php echo $shortdesc; ?>
+											</div>
+										<?php } ?>
+									</div>	
+								</div>
+
+								<?php if( have_rows('benefits', get_the_id()) ) { ?>
+									<div class="property__benefits hidedesktop">
+										<?php while( have_rows('benefits', get_the_id()) ): the_row(); 
+											$title = get_sub_field('benefits_title');
+											$image = get_sub_field('benefits_icon');
+										?>
+										<div class="property__benefits-item">
+											<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+											<?php if($title) { ?><p class="text--green"><strong><?php echo $title; ?></strong></p><?php } ?>
+										</div>
+										<?php endwhile; ?>
+									</div>
+								<?php } ?>
+								
+
+								<?php if( have_rows('benefits', get_the_id()) ) { ?>
+									<div class="property__benefits hidemobile">
+										<?php while( have_rows('benefits', get_the_id()) ): the_row(); 
+											$title = get_sub_field('benefits_title');
+											$image = get_sub_field('benefits_icon');
+										?>
+										<div class="property__benefits-item">
+											<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+											<?php if($title) { ?><p class="text--green"><strong><?php echo $title; ?></strong></p><?php } ?>
+										</div>
+										<?php endwhile; ?>
+									</div>
+								<?php } ?>
+								<a href="<?php esc_url(the_permalink() ); ?>" class="btn">Find out more</a>
+							</div>
+						</div>
+					<?php endwhile; ?>
+					</div>
+				<?php } ?>
+
 			</div>
 		</div>
 	</article>

e0dd901089e2671aecccc1a79e0fd4149b33cc4d
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-10-26 11:20:38 +0100

    Add tax

--- wp-content/plugins/site-functionality/taxonomies.php
+++ wp-content/plugins/site-functionality/taxonomies.php
@@ -65,6 +65,37 @@ function origin_taxonomies() {
 	);
 	register_taxonomy( $taxonomy_name, $post_type, $args );
 
+	$taxonomy_name		= "amenities";
+	$single_name		= "Amenity";
+	$plural_name		= "Amenities";
+	$post_type			= "accommodation";
+	$gutenberg_enabled	= false;
+
+	$labels = array(
+	    'name'              => $plural_name,
+	    'singular_name'     => $single_name,
+	    'search_items'      => 'Search ' . $plural_name,
+	    'all_items'         => 'All ' . $plural_name,
+	    'parent_item'       => 'Parent ' . $single_name,
+	    'parent_item_colon' => 'Parent ' . $single_name . ':',
+	    'edit_item'         => 'Edit ' . $single_name,
+	    'update_item'       => 'Update ' . $single_name,
+	    'add_new_item'      => 'Add New ' . $single_name,
+	    'new_item_name'     => 'New ' . $single_name . ' Name',
+	    'menu_name'         => $plural_name
+	);
+
+	$args = array(
+	    'labels'            => $labels,
+	    'hierarchical'      => true,
+	    'show_ui'           => true,
+	    'show_admin_column' => true,
+	    'query_var'         => true,
+		'show_in_rest'      => $gutenberg_enabled,
+	    'rewrite'           => array( 'slug' => '' ),
+	);
+	register_taxonomy( $taxonomy_name, $post_type, $args );
+
 
 	$taxonomy_name		= "location";
 	$single_name		= "Location";

9eb95cdc34af3a9e5fe5b224497580514a7e5ada
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-12-13 11:54:32 +0000

    Create grid plus mobile styling

--- wp-content/themes/joint-theme/lib/gutenberg.php
+++ wp-content/themes/joint-theme/lib/gutenberg.php
@@ -84,6 +84,7 @@ function joint_allowed_block_types( $allowed_blocks ) {
 		'acf/custom-block-joint-22',
 		'acf/custom-block-joint-222',
 		'acf/custom-block-joint-223',
+		'acf/custom-block-joint-224',
 		'acf/custom-block-joint-333',
 		
 		
